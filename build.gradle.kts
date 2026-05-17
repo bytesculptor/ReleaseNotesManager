c5d46d97ba92abbf2e61c707c8e975822dc6b931
Byte Sculptor <bytesculptor@gmail.com>
2026-05-17 13:32:09 +0200

    Libs updates

--- build.gradle.kts
+++ build.gradle.kts
@@ -20,7 +20,10 @@ plugins {
   alias(libs.plugins.kotlin.jvm)
 }
 
-java {
-  sourceCompatibility = JavaVersion.VERSION_17
-  targetCompatibility = JavaVersion.VERSION_17
+kotlin {
+  jvmToolchain(21)
+}
+
+dependencies {
+  compileOnly(gradleApi())
 }
